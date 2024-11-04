82fdbffc6b230c788bc94a7f223b80b53136a6f1
Johnathon <me@johnathon.nz>
2024-11-04 21:08:35 +1300

    das
    dsa

--- Yu-Gi-Oh_Loader/Game.h
+++ Yu-Gi-Oh_Loader/Game.h
@@ -6,11 +6,14 @@ public:
 	static BOOL Locate();
 	static BOOL Start();
 
+	static BOOL Check(LPSTR Path);
 	static BOOL Start(LPSTR CustomPath);
+	static void LookForPlugins();
+
 	static void Set_GamePath(CHAR Path[MAX_PATH]);
 
 private:
 	static TCHAR gGamePath[MAX_PATH];
 	static TCHAR gGameLocation[MAX_PATH];
-	static TCHAR gDllLocation[MAX_PATH];
+	static LPCSTR gDlls[MAX_PATH];
 };
