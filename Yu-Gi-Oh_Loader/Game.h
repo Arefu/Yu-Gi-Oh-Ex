c5d0ec7b98c3e49de7ce1f207bac30002034f17e
Johnathon <me@johnathon.nz>
2024-11-07 21:53:23 +1300

    Start Seperating and reimplimenting proper imgui

--- Yu-Gi-Oh_Loader/Game.h
+++ Yu-Gi-Oh_Loader/Game.h
@@ -1,19 +1,57 @@
 #pragma once
+#include <vector>
 
 static class Game
 {
 public:
+	/// <summary>
+	/// Looks for the Game's path in the registry
+	/// </summary>
+	/// <returns>TRUE if located, FALSE if not.</returns>
 	static BOOL Locate();
-	static BOOL Start();
 
+	/// <summary>
+	/// Checks if the game's path is valid by looking for the game's executable.
+	/// </summary>
+	/// <param name="Path">The location to check</param>
+	/// <returns>TRUE if the EXE is there.</returns>
 	static BOOL Check(LPSTR Path);
-	static BOOL Start(LPSTR CustomPath);
+	
+	/// <summary>
+	/// Starts the game
+	/// </summary>
+	/// <param name="Plugins">Enable Plugin Loading</param>
+	/// <returns>TRUE if Process Created</returns>
+	static BOOL Start(bool Plugins);
+
+	/// <summary>
+	/// Starts the game with a custom path
+	/// </summary>
+	/// <param name="CustomPath">Custom Path to load from</param>
+	/// <param name="Plugins">Enable Plugin Loading</param>
+	/// <returns>TRUE if Process Created</returns>
+	static BOOL Start(LPSTR CustomPath, bool Plugins);
+
+	/// <summary>
+	/// Looks for plugins in the Plugins folder from the Loader's directory
+	/// </summary>
 	static void LookForPlugins();
 
+	/// <summary>
+	/// Sets the game's path to the specified path
+	/// </summary>
+	/// <param name="Path">Force the game path</param>
 	static void Set_GamePath(CHAR Path[MAX_PATH]);
+
+
+	/// <summary>
+	/// The plugins to load, formatted for Detours.
+	/// </summary>
+	static std::vector<LPCSTR> gPlugins;
 
 private:
 	static TCHAR gGamePath[MAX_PATH];
 	static TCHAR gGameLocation[MAX_PATH];
-	static LPCSTR gDlls[MAX_PATH];
+	static std::vector<std::string> gDlls;
+	
 };
