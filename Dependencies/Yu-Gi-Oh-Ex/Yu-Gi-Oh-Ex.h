e7645a274ebb9da2c95cdb8ef7c9d564175cf36c
Johnathon <me@johnathon.nz>
2024-11-24 15:29:26 +1300

    The heart of the cards is now dead
    Yu-Gi-Oh-GUI shows you your upcoming deck IDs, as well as hand, P2 NYI.... (soon?)
    
    Bonds checking is applied based on what game thinks you have currently. mess with at your peril.

--- Dependencies/Yu-Gi-Oh-Ex/Yu-Gi-Oh-Ex.h
+++ Dependencies/Yu-Gi-Oh-Ex/Yu-Gi-Oh-Ex.h
@@ -1,7 +1,13 @@
-#include <WTypesbase.h>
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <vector>
+
+#include "Cards.h"
+
+using std::vector;
 
 #define PLAYER_ONE 0x143497C40
-#define PLAYER_TWO = 0x1434989D4
+#define PLAYER_TWO 0x1434989D4
 
 namespace YuGiOhEx
 {
@@ -16,13 +22,39 @@ private:
 
 public:
 	Player(__int64  Player);
-	__int64 Get_Player();
-
+	
 	INT Get_NumberOfCardsInHand();
+	SHORT Get_CardInHand(INT Slot);
+	vector<SHORT> Get_CardsInHand();
+
+
 	INT Get_NumberOfCardsInDeck();
+	SHORT Get_CardInDeck(INT Slot);
+	vector<SHORT> Get_CardsInDeck();
+
 	INT Get_NumberOfCardsInGraveYard();
+	SHORT Get_CardInGraveYard(INT Slot);
+	vector<SHORT> Get_CardsInGraveYard();
+
+	INT Get_NumberOfCardsInExtraPile();
+	SHORT Get_CardInExtraPile(INT Slot);
+	vector<SHORT> Get_CardsInExtraPile();
+
 	INT Get_NumberOfDiscardPile();
+	SHORT Get_CardInDiscardPile(INT Slot);
+	vector<SHORT> Get_CardsInDiscardPile();
+
 
 	SHORT Get_MonsterInSlot(INT Slot);
 };
 
+class YuGiOh
+{
+private:
+
+public:
+	static BYTE Get_IsDuelTutorial();
+
+
+	static INT Get_SelectedSlotOnDuelMat();
+};
