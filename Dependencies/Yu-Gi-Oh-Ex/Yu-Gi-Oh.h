2b991f3b408a2ec728c00672a91cbcaa5244cc48
Johnathon <me@johnathon.nz>
2024-12-14 21:02:28 +1300

    clean

--- Dependencies/Yu-Gi-Oh-Ex/Yu-Gi-Oh.h
+++ Dependencies/Yu-Gi-Oh-Ex/Yu-Gi-Oh.h
@@ -3,7 +3,7 @@ namespace YGO
 	namespace CardEffects
 	{
 		//DealDamageToLP
-		typedef long long DealDamageToLP(unsigned __int16* Amount);
+		typedef long DealDamageToLP(unsigned __int16* ID);
 		DealDamageToLP* _DealDamageToLP = (DealDamageToLP*)0x14015E570;
 
 
--- Tools/WolfX/WolfX State/WolfX_UI_State.cs
+++ Tools/WolfX/WolfX State/WolfX_UI_State.cs
@@ -7,13 +7,9 @@ namespace WolfX
         internal static class State
         {
             internal static string? WorkingDirectory { get; set; }
-            internal static bool? IsLoaded { get; set; }
-            public static bool HasArchiveOpen { get; set; }
 
             public static Language Language { get; set; } = Language.English;
-            public static List<Card> Cards { get; set; } = new List<Card>();
 
-            public static int CardIndex = 1;
         }
     }
 }
