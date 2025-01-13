804e8f658035edde1c8e6890a67e27dbaaf42ac4
Johnathon <me@johnathon.nz>
2025-01-13 23:38:43 +1300

    No more pesky Janken

--- Yu-Gi-Oh_Loader Plugins/Yu-Gi-Oh-Tracer/YGO_DuelSetup.h
+++ Yu-Gi-Oh_Loader Plugins/Yu-Gi-Oh-Tracer/YGO_DuelSetup.h
@@ -4,6 +4,7 @@ namespace YGO
 {
 	namespace DuelSetup
 	{
-		bool _D_Get_IsRoundBasedDuel(__int64 a1);
+		bool _D_MP_Get_IsRoundBasedDuel(__int64 a1);
+		__int64 _D_DoJankenMiniGame(__int64 a1, float a2);
 	}
 }
