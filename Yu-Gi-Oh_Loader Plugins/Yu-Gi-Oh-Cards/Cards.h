89306641f0bde4bdc883ab21f5dab94a32f20cea
Johnathon <me@johnathon.nz>
2025-04-28 17:26:12 +1200

    revert of card and intl to vector
    I'm going to try out creating a vector only after they are copied to the interal arary, but hopefully before to many links are setup, but then override all static address checks to mine

--- Yu-Gi-Oh_Loader Plugins/Yu-Gi-Oh-Cards/Cards.h
+++ Yu-Gi-Oh_Loader Plugins/Yu-Gi-Oh-Cards/Cards.h
@@ -23,17 +23,15 @@ public:
 		int Field_13;
 	};
 	
-	 std::vector<uint16_t> _CARD_IDs;
-	 std::vector<uint16_t> _INTERNAL_IDs;
-	 std::vector<MEMORY_CARD_PROP> _CARD_PROPS;
-
+	static uint16_t _INTERNAL_IDs[11072];
+	static uint16_t _CARD_IDs[10168];
+	static std::vector<MEMORY_CARD_PROP> _CARD_PROPS;
 
 	static MEMORY_CARD_PROP* __fastcall Get_CardProps(unsigned int a1);
 	static __int64 __fastcall Get_InternalID(__int16 a1);
 	static __int64 __fastcall Get_CardID(__int16 a1);
 
 	static __int64 Setup_CardPropTable();
-
 
 private:
 };
