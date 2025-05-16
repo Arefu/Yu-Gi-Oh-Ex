f3000b74cd09887cab8f75e26c4a53fac3c10704
Johnathon <me@johnathon.nz>
2025-05-16 22:11:07 +1200

    chore: verifying a certain function via tracer

--- Yu-Gi-Oh_Loader Plugins/Yu-Gi-Oh-Tracer/dllmain.cpp
+++ Yu-Gi-Oh_Loader Plugins/Yu-Gi-Oh-Tracer/dllmain.cpp
@@ -41,13 +41,22 @@ BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpRese
     }
     return TRUE;
 }
-
-
+uintptr_t te = 0x1407EB9E0;
+typedef __int64(__fastcall* sub_14076BFC0)(unsigned __int16 a1, int a2);
+__int64 __fastcall I_Dont_Know(unsigned short a1, int a2)
+{
+    //__int64 __fastcall sub_1407EB9E0(unsigned __int16 a1, int a2)
+   // auto res = -((sub_14076BFC0)te)(a1, a2);
+  //  std::cout << a1 << " - " << res;
+    
+    return 0;
+}
 
 void AddTrace()
 {
     Tracer::Traces.push_back({ "YGO::DuelSetup::MP_Get_IsRoundBasedDuel", (void*)0x1407F1E80, (void*)YGO::DuelSetup::_D_MP_Get_IsRoundBasedDuel });
 	Tracer::Traces.push_back({ "YGO::DuelUtility::Get_TutorialDuelIndex", (void*)0x140769180, (void*)YGO::DuelUtility::_D_Get_TutorialDuelIndex });
+  //  Tracer::Traces.push_back({ "I don't know!", (void*)te, (void*)I_Dont_Know });
 }
 
 void SetupTrace()
@@ -59,8 +68,13 @@ void SetupTrace()
     for (auto& _Trace : Tracer::Traces)
     {
         WriteLog("Tracing Execution Of : " + _Trace._Name, MODULE_NAME, 1);
-        DetourAttach((PVOID*)&_Trace._Address, _Trace._Detour);
+        std::cout << "Tracing " << _Trace._Address << std::endl;
+        auto res = DetourAttach((PVOID*)&_Trace._Address, _Trace._Detour);
+
+        std::cout << "RES: " << res << std::endl;
     }
+
+    DetourAttach((PVOID*)&te, I_Dont_Know);
 
 	DetourTransactionCommit();
 }
