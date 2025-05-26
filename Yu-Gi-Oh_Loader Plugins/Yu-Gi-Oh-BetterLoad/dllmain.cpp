910d90d66f2557a26b7963bc8a871915dc54b34d
Johnathon <me@johnathon.nz>
2025-05-26 17:44:10 +1200

    build changes

--- Yu-Gi-Oh_Loader Plugins/Yu-Gi-Oh-BetterLoad/dllmain.cpp
+++ Yu-Gi-Oh_Loader Plugins/Yu-Gi-Oh-BetterLoad/dllmain.cpp
@@ -3,11 +3,14 @@
 #include <cstdio>
 #include <fstream>
 #include <detours.h>
+#include <format>
+#include <io.h>
 
+#include <DbgHelp.h>
+#include <fstream>
 #include "Logger.h"
 
 uintptr_t LoadArchive = 0x14080D3D0;
-uintptr_t te = 0x140766F70;
 
 bool AllowMultiInstance = false;
 
@@ -28,7 +31,7 @@ HANDLE WINAPI HookCreateMutex(LPSECURITY_ATTRIBUTES lpMutexAttributes, BOOL bIni
 
 typedef FILE* (__cdecl* fopen_t)(const char* filename, const char* mode);
 typedef errno_t(__cdecl* fopen_s_t)(FILE** file, const char* filename, const char* mode);
-typedef size_t(__cdecl* fread_t)(void* buffer, size_t size, size_t count, FILE* stream);
+typedef size_t(__cdecl* fread_t)(void* ptr, size_t size, size_t count, FILE* stream);
 
 fopen_t original_fopen = nullptr;
 fopen_s_t original_fopen_s = nullptr;
@@ -47,19 +50,38 @@ Logger::WriteLog("Requested file (fopen_s): " + std::string(filename), MODULE_NA
 	return original_fopen_s(file, filename, mode);
 }
 
+size_t __cdecl hooked_fread(void* ptr, size_t size, size_t count, FILE* stream)
+{
+	// Just log and call original  int fd = _fileno(file);
+	Logger::WriteLog(std::format("Reading From {}", count),MODULE_NAME,0);
+	return original_fread(ptr, size, count, stream);
+}
+
+LONG WINAPI CrashHandler(EXCEPTION_POINTERS* ExceptionInfo) {
+
+	std::ofstream log("crash_log.txt");
+	log << "Crash Address: 0x" << std::hex << (uintptr_t)ExceptionInfo->ExceptionRecord->ExceptionAddress << "\n";
+	log << "Exception Code: 0x" << std::hex << ExceptionInfo->ExceptionRecord->ExceptionCode << "\n";
+	log.close();
+	Sleep(2000000000000);
+	return EXCEPTION_EXECUTE_HANDLER;
+}
+
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
 	Logger::SetupLogger();
+	SetUnhandledExceptionFilter(CrashHandler);
 
-	HMODULE stdio = GetModuleHandleA("api-ms-win-crt-stdio-l1-1-0.dll.dll");
-	original_fopen = (fopen_t)GetProcAddress(stdio, "fopen");
-	original_fopen_s = (fopen_s_t)GetProcAddress(stdio, "fopen_s");
-	original_fread = (fread_t)GetProcAddress(stdio, "fread");
 
 
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
+		HMODULE stdio = GetModuleHandleA("api-ms-win-crt-stdio-l1-1-0.dll.dll");
+		original_fopen = (fopen_t)GetProcAddress(stdio, "fopen");
+		original_fopen_s = (fopen_s_t)GetProcAddress(stdio, "fopen_s");
+		original_fread = (fread_t)GetProcAddress(stdio, "fread");
+
 		GetPrivateProfileStringA("Yu-Gi-Oh-BetterLoad", "Archive", "YGO_2020", _Archive, 255, ".\\Config.ini");
 		
 		DetourTransactionBegin();
@@ -68,15 +90,15 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
 		DetourAttach(&(PVOID&)LoadArchive, _hLoadArchive);
 		DetourAttach(&(PVOID&)original_fopen, hooked_fopen);
 		DetourAttach(&(PVOID&)original_fopen_s, hooked_fopen_s);
-
+		std::cout << "FREAD: " << original_fread << std::endl;
+		DetourAttach(&(PVOID&)original_fread, hooked_fread);
+		
 		if (GetPrivateProfileIntA("Yu-Gi-Oh-BetterLoad", "AllowMultiInstance", 0, ".\\Config.ini") == 1)
 			DetourAttach(&(PVOID&)pCreateMutex, HookCreateMutex);
 		
 		DetourTransactionCommit();
-		break;
-	case DLL_THREAD_ATTACH:
-	case DLL_THREAD_DETACH:
-	case DLL_PROCESS_DETACH:
+		std::cout << "FREAD: " << original_fread << std::endl;
+
 		break;
 	}
 	return TRUE;
