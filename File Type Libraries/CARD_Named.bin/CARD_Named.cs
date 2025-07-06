aac7f733d6bd8eed918be03c51d92395e5a5c1cd
Johnathon <johnathonkm@gmail.com>
2025-07-06 20:50:38 +1200

    feat: CARD_Named Pass and Prop changes

---
+++ File Type Libraries/CARD_Named.bin/CARD_Named.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+namespace CARD_Named
+{
+    public static class CARD_Named
+    {
+        public static int NUMBER_OF_ARCHETYPES;
+        public static int NUMBER_OF_CARDS_WITH_ARCHETYPES;
+
+        public static Dictionary<int, List<short>> _CardIDsAndArchetype = [];
+
+        public enum Archetype
+        {
+            Toon = 0x0,
+            Archfiend = 0x1
+        }
+
+        public static Dictionary<int, List<short>> Load(string Path)
+        {
+            var Archetype = new Dictionary<int, List<short>>();
+            using var Reader = new BinaryReader(File.Open(Path, FileMode.Open, FileAccess.Read));
+
+            var NUMBER_OF_ARCHETYPES = Reader.ReadUInt16();
+            var NUMBER_OF_CARDS_WITH_ARCHETYPES = (int)Reader.ReadUInt16();
+
+            
+
+            return Archetype;
+        }
+
+        public static void Save()
+        {
+
+        }
+    }
+}
---
+++ File Type Libraries/CARD_Pass/CARD_Pass.cs
@@ -0,0 +1,27 @@
+﻿namespace CARD_Pass
+{
+    public static class CARD_Pass
+    {
+        public static List<int> _Passwords = [];
+
+        public static List<int> Load(string Path)
+        {
+            var Passwords = new List<int>();
+
+            using var Reader = new BinaryReader(File.Open(Path, FileMode.Open, FileAccess.Read));
+
+            while(Reader.BaseStream.Position != Reader.BaseStream.Length)
+            {
+                Passwords.Add(Reader.ReadInt32());
+            }
+
+            _Passwords = Passwords;
+            return Passwords;
+        }
+
+        public static void Save(List<int> Passwords)
+        {
+
+        }
+    }
+}
