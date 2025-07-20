30a64145f24238e9a04894fc41ace1c7cb082845
Johnathon <johnathonkm@gmail.com>
2025-07-20 15:23:45 +1200

    fix: Overhaul of FUll card adder pending design

--- Tools/WolfX/WolfX/File Type UI/Card Replacer (Full).Designer.cs
+++ Tools/WolfX/WolfX/File Type UI/Card Replacer (Full).Designer.cs
@@ -28,124 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            groupBox2 = new GroupBox();
-            label2 = new Label();
-            CARDREP_BTN_UpdateCard = new Button();
-            CARDREP_TB_DEF = new TextBox();
-            CARDREP_TB_ATK = new TextBox();
-            CARDREP_LBL_NewCardDesc = new Label();
-            CARDREP_CB_NewCardSelector = new ComboBox();
-            CARDREP_PB_NewCardImage = new PictureBox();
-            groupBox2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)CARDREP_PB_NewCardImage).BeginInit();
+            YDCFull_LV_ListOfAllCards = new ListView();
+            YDCFull_BTN_Save = new Button();
+            YDCFull_BTN_Cancel = new Button();
             SuspendLayout();
             // 
-            // groupBox2
+            // YDCFull_LV_ListOfAllCards
             // 
-            groupBox2.Controls.Add(label2);
-            groupBox2.Controls.Add(CARDREP_BTN_UpdateCard);
-            groupBox2.Controls.Add(CARDREP_TB_DEF);
-            groupBox2.Controls.Add(CARDREP_TB_ATK);
-            groupBox2.Controls.Add(CARDREP_LBL_NewCardDesc);
-            groupBox2.Controls.Add(CARDREP_CB_NewCardSelector);
-            groupBox2.Controls.Add(CARDREP_PB_NewCardImage);
-            groupBox2.Location = new Point(-1, 0);
-            groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(310, 441);
-            groupBox2.TabIndex = 1;
-            groupBox2.TabStop = false;
-            groupBox2.Text = "New Card";
+            YDCFull_LV_ListOfAllCards.Location = new Point(12, 12);
+            YDCFull_LV_ListOfAllCards.Name = "YDCFull_LV_ListOfAllCards";
+            YDCFull_LV_ListOfAllCards.Size = new Size(680, 388);
+            YDCFull_LV_ListOfAllCards.TabIndex = 0;
+            YDCFull_LV_ListOfAllCards.UseCompatibleStateImageBehavior = false;
             // 
-            // label2
+            // YDCFull_BTN_Save
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(87, 410);
-            label2.Name = "label2";
-            label2.Size = new Size(12, 15);
-            label2.TabIndex = 7;
-            label2.Text = "/";
+            YDCFull_BTN_Save.Location = new Point(617, 406);
+            YDCFull_BTN_Save.Name = "YDCFull_BTN_Save";
+            YDCFull_BTN_Save.Size = new Size(75, 23);
+            YDCFull_BTN_Save.TabIndex = 1;
+            YDCFull_BTN_Save.Text = "Save";
+            YDCFull_BTN_Save.UseVisualStyleBackColor = true;
             // 
-            // CARDREP_BTN_UpdateCard
+            // YDCFull_BTN_Cancel
             // 
-            CARDREP_BTN_UpdateCard.Location = new Point(223, 405);
-            CARDREP_BTN_UpdateCard.Name = "CARDREP_BTN_UpdateCard";
-            CARDREP_BTN_UpdateCard.Size = new Size(75, 23);
-            CARDREP_BTN_UpdateCard.TabIndex = 6;
-            CARDREP_BTN_UpdateCard.Text = "Confirm";
-            CARDREP_BTN_UpdateCard.UseVisualStyleBackColor = true;
-            CARDREP_BTN_UpdateCard.Click += CARDREP_BTN_UpdateCard_Click;
-            // 
-            // CARDREP_TB_DEF
-            // 
-            CARDREP_TB_DEF.Location = new Point(105, 406);
-            CARDREP_TB_DEF.Name = "CARDREP_TB_DEF";
-            CARDREP_TB_DEF.ReadOnly = true;
-            CARDREP_TB_DEF.Size = new Size(75, 23);
-            CARDREP_TB_DEF.TabIndex = 5;
-            // 
-            // CARDREP_TB_ATK
-            // 
-            CARDREP_TB_ATK.Location = new Point(6, 406);
-            CARDREP_TB_ATK.Name = "CARDREP_TB_ATK";
-            CARDREP_TB_ATK.ReadOnly = true;
-            CARDREP_TB_ATK.Size = new Size(75, 23);
-            CARDREP_TB_ATK.TabIndex = 4;
-            // 
-            // CARDREP_LBL_NewCardDesc
-            // 
-            CARDREP_LBL_NewCardDesc.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            CARDREP_LBL_NewCardDesc.Location = new Point(6, 182);
-            CARDREP_LBL_NewCardDesc.Name = "CARDREP_LBL_NewCardDesc";
-            CARDREP_LBL_NewCardDesc.Size = new Size(292, 220);
-            CARDREP_LBL_NewCardDesc.TabIndex = 2;
-            CARDREP_LBL_NewCardDesc.Text = "label2";
-            // 
-            // CARDREP_CB_NewCardSelector
-            // 
-            CARDREP_CB_NewCardSelector.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
-            CARDREP_CB_NewCardSelector.AutoCompleteSource = AutoCompleteSource.ListItems;
-            CARDREP_CB_NewCardSelector.FormattingEnabled = true;
-            CARDREP_CB_NewCardSelector.Location = new Point(6, 22);
-            CARDREP_CB_NewCardSelector.Name = "CARDREP_CB_NewCardSelector";
-            CARDREP_CB_NewCardSelector.Size = new Size(292, 23);
-            CARDREP_CB_NewCardSelector.TabIndex = 1;
-            CARDREP_CB_NewCardSelector.SelectedIndexChanged += CARDREP_CB_NewCardSelector_SelectedIndexChanged;
-            // 
-            // CARDREP_PB_NewCardImage
-            // 
-            CARDREP_PB_NewCardImage.Location = new Point(6, 51);
-            CARDREP_PB_NewCardImage.Name = "CARDREP_PB_NewCardImage";
-            CARDREP_PB_NewCardImage.Size = new Size(292, 128);
-            CARDREP_PB_NewCardImage.SizeMode = PictureBoxSizeMode.Zoom;
-            CARDREP_PB_NewCardImage.TabIndex = 0;
-            CARDREP_PB_NewCardImage.TabStop = false;
+            YDCFull_BTN_Cancel.Location = new Point(536, 406);
+            YDCFull_BTN_Cancel.Name = "YDCFull_BTN_Cancel";
+            YDCFull_BTN_Cancel.Size = new Size(75, 23);
+            YDCFull_BTN_Cancel.TabIndex = 2;
+            YDCFull_BTN_Cancel.Text = "Cancel";
+            YDCFull_BTN_Cancel.UseVisualStyleBackColor = true;
             // 
             // Card_Changer
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(304, 441);
-            Controls.Add(groupBox2);
+            ClientSize = new Size(704, 441);
+            Controls.Add(YDCFull_BTN_Cancel);
+            Controls.Add(YDCFull_BTN_Save);
+            Controls.Add(YDCFull_LV_ListOfAllCards);
             FormBorderStyle = FormBorderStyle.FixedDialog;
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "Card_Changer";
             StartPosition = FormStartPosition.CenterParent;
-            Text = "Deck Editor - Card Replacer";
-            groupBox2.ResumeLayout(false);
-            groupBox2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)CARDREP_PB_NewCardImage).EndInit();
+            Text = "Card Replacer (Full)";
             ResumeLayout(false);
         }
 
         #endregion
-        private GroupBox groupBox2;
-        private Label CARDREP_LBL_NewCardDesc;
-        private ComboBox CARDREP_CB_NewCardSelector;
-        private PictureBox CARDREP_PB_NewCardImage;
-        private Button CARDREP_BTN_UpdateCard;
-        private TextBox CARDREP_TB_DEF;
-        private TextBox CARDREP_TB_ATK;
-        private Label label2;
+
+        private ListView YDCFull_LV_ListOfAllCards;
+        private Button YDCFull_BTN_Save;
+        private Button YDCFull_BTN_Cancel;
     }
 }
--- Tools/WolfX/WolfX/File Type UI/Card Replacer (Full).cs
+++ Tools/WolfX/WolfX/File Type UI/Card Replacer (Full).cs
@@ -6,73 +6,9 @@ namespace WolfX.WolfX.File_Type_UI
     public partial class Card_Changer : Form
     {
         public bool HasLoaded = false;
-        public Card_Changer()
+        public Card_Changer(List<ZIB_Item> IllustrationImages, List<CARD_Card> Cards)
         {
             InitializeComponent();
-            this.Text = "Card Replacer (Full)";
-        }
-
-
-        public void LoadCards(CARDS_INFO.CARD_Language Language, string Path)
-        {
-            HasLoaded = true;
-            if (Path == null || Path.Length == 0)
-            {
-                using (var OpenFile = new OpenFileDialog())
-                {
-                    OpenFile.Filter = $"{Language} Card Indx File|Card_Indx_{Language.ToString()[0]}.bin|All Indx Files (*.bin)|*.bin";
-                    OpenFile.Title = "Open Cards Indx File";
-
-                    var Res = OpenFile.ShowDialog();
-                    if (Res != DialogResult.OK)
-                    {
-                        MessageBox.Show("Please Select a Cards Indx File", "No Cards Indx File Selected", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        return;
-                    }
-                    if (OpenFile.SafeFileName.StartsWith("CARD_Indx") != true)
-                    {
-                        MessageBox.Show("Please Select a Cards Indx File", "Incorrect File Selected", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        return;
-                    }
-
-                }
-            }
-            if (CARDS_Cards.Setup_CardBinder($"{Path}\\bin\\CARD_Indx_{Language.ToString()[0]}.bin", (CARDS_INFO.CARD_Language)State.Language) == false)
-            {
-                MessageBox.Show("Failed to Setup Card Binder\nCheck Yu-Gi-Oh-Ex Wiki!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
-
-            CARDS_Cards.LoadCardInfo();
-            CARDS_Cards.LoadCardProps();
-
-            CARDREP_CB_NewCardSelector.DataSource = CARDS_Cards.Cards.Select(Select => Select.Name).ToList();
-        }
-
-
-        private void CARDREP_CB_NewCardSelector_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if (CARDS_Cards.Cards.Count == 0)
-                return;
-
-            var ID = CARDS_Cards.Cards.Where(Card => Card.Name == CARDREP_CB_NewCardSelector.Text).First();
-            if (ID != null && ID.ID != 0)
-            {
-
-            }
-
-            CARDREP_TB_ATK.Text = ID.Attack.ToString();
-            CARDREP_TB_DEF.Text = ID.Defense.ToString();
-
-            CARDREP_LBL_NewCardDesc.Text = ID.Desc;
-        }
-
-        public CARD_Card Card = new CARD_Card();
-        private void CARDREP_BTN_UpdateCard_Click(object sender, EventArgs e)
-        {
-            Card = CARDS_Cards.Cards.Where(Card => Card.Name == CARDREP_CB_NewCardSelector.Text).First();
-            DialogResult = DialogResult.OK;
-            Close();
         }
     }
 }
