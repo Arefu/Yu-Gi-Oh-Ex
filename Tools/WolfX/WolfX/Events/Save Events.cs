91a29f3a3266552d6629c205176b2468ccc560fa
Johnathon <johnathonkm@gmail.com>
2025-07-20 15:24:23 +1200

    feat: Start of save editor events

---
+++ Tools/WolfX/WolfX/Events/Save Events.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Types;
+using WolfX.WolfX.File_Type_UI;
+
+using savegame;
+
+namespace WolfX
+{
+    public partial class WolfUI
+    {
+        private void SaveGame_BTN_OpenSave_Click(object sender, EventArgs e)
+        {
+            using var SaveGameFile = new OpenFileDialog();
+            SaveGameFile.Title = "Select \"savegame.dat\"";
+
+            
+            if (SaveGameFile.ShowDialog() != DialogResult.OK)
+                MessageBox.Show("Please \"savegame.dat\"", "Invalid File", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            SaveGame.Load(SaveGameFile.FileName);
+
+            SaveGame.Load_Decks();
+        }
+    }
+}
