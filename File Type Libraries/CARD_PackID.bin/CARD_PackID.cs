4dc8f2c26d6d8bf091e1d84ceb4c8a387e6f7b49
Johnathon <johnathonkm@gmail.com>
2025-07-30 14:06:43 +1200

    fix: Fixed filename (oops double commit)

---
+++ File Type Libraries/CARD_PackID.bin/CARD_PackID.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace CARD_PackID
+{
+    public static class Card_PackID
+    {
+        public static List<string> _CardNumbers = [];
+
+        public static void Load(string Path)
+        {
+            using var Reader = new BinaryReader(File.Open(Path, FileMode.Open, FileAccess.Read));
+
+            while (Reader.BaseStream.Position < Reader.BaseStream.Length)
+            {
+                var Bytes = Reader.ReadBytes(16);
+
+                _CardNumbers.Add(Encoding.ASCII.GetString(Bytes));
+            }
+        }
+
+        public static void Save()
+        {
+        }
+    }
+}
