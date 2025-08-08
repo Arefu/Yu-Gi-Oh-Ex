11e909d1440c318dc7c9e6e5be6698e17d3d4ec1
Johnathon <JohnathonKM@Gmail.com>
2025-08-08 21:28:00 +1200

    feat: Card_named load and archetype functions

--- File Type Libraries/CARD_Named.bin/CARD_Named.cs
+++ File Type Libraries/CARD_Named.bin/CARD_Named.cs
@@ -1,4 +1,6 @@
-﻿namespace CARD_Named
+﻿using System.Collections;
+
+namespace CARD_Named
 {
     public static class Card_Named
     {
@@ -9,13 +11,127 @@
 
         public enum Archetype
         {
-            Toon = 0x0,
-            Archfiend = 0x1
+            CardsLikeToon = 1,
+            CardsLikeArchfiend = 2,
+            CardsLikeGravekeeper = 3,
+            CardsLikeGuardian = 4,
+            CardsLikeDarkScorpion = 5,
+            CardsLikeAmazoness = 6,
+            CardsLikeNinja = 7,
+            CardsLikeLV = 8,
+            CardsLikeElementalHero = 9,
+            CardsLikeDestinyHero =10,
+            CardsLikeNeos = 11,
+            CardsLikeNeoAndNeoSpacian = 12,
+            CardsLikeNeoFusion= 13,
+            CardsLikeOjama = 14,
+            CardsLikeBatteryman = 15,
+            CardsLikeDarkWorld = 16,
+            CardsLikeBES = 17,
+            CardsLikeAncientGear = 18,
+            CardsLikeSphinx = 19,
+            CardsLikeMachina = 20,
+            CardsLikeHarpie = 21,
+            CardsLikeRoid = 22,
+            CardsLikeVehicroid = 23,
+            CardsLikeNeoSpacian = 24,
+            CardsLikeChrysalis = 25,
+            CardsLikeAlien = 26,
+            CardsLikePhantomBeast = 27,
+            CardsLikeHero = 28,
+            CardsLikeAllureQueen = 29,
+            CardsLikeGadget = 30,
+            CardsLikeSixSamurai = 31,
+            CardsLikeCrystalBeast = 32,
+            CardslikeVolcanic = 33,
+            CardsLikeBlazeAccelerator = 34,
+            CardsLikeVenom = 35,
+            CardsLikeCloudian = 36,
+            CardslikeGladiatorBeast = 37,
+            CardsLikeGladiatorBeastsBattle = 38,
+            CardsLikeBambooSword = 39,
+            CardsLikeEvilHero = 40,
+            //41
+            CardsLikeArcanaForce = 42,
+            //43
+            CardsLikeSkyblaster = 43,
+            CardsLikeExodia = 44,
+            CardsLikeUltimateCrystal = 45,
+            CardsLikeCyberDragon = 46,
         }
 
-        public static Dictionary<int, List<short>> Load(string path)
+        public static string Get_ArchetypeNameFromID(Archetype archetype)
         {
-            return [];
+            switch (archetype)
+            {
+                case Archetype.CardsLikeToon:
+                    return "Toon";
+                case Archetype.CardsLikeArchfiend:
+                    return "Archfiend";
+                case Archetype.CardsLikeGravekeeper:
+                    return "Gravekeeper";
+                case Archetype.CardsLikeGuardian:
+                    return "Guardian";
+                case Archetype.CardsLikeDarkScorpion:
+                    return "Dark Scorpion";
+                case Archetype.CardsLikeAmazoness:
+                    return "Amazoness";
+                case Archetype.CardsLikeNinja:
+                    return "Ninja";
+                case Archetype.CardsLikeLV:
+                    return "LV";
+                case Archetype.CardsLikeElementalHero:
+                    return "Elemental HERO";
+                case Archetype.CardsLikeNeos:
+                    return "Neos";
+                case Archetype.CardsLikeNeoAndNeoSpacian:
+                    return "Neos AND Neo-Spacian";
+                case Archetype.CardsLikeNeoFusion:
+                    return "Neo (Change Me)";
+                case Archetype.CardsLikeOjama:
+                    return "Ojama";
+                default:
+
+                    return "None";
+            }
+        }
+
+        public static Dictionary<Archetype, List<int>> CardsInArchetype = new Dictionary<Archetype, List<int>>();
+
+        public static void Load(string path)
+        {
+            var Reader = new BinaryReader(File.Open(path, FileMode.Open, FileAccess.Read));
+
+            var Archetypes = Reader.ReadUInt16();
+            var NumberOfCards = Reader.ReadUInt16();
+
+            //Advance Reader.
+            Reader.BaseStream.Position = 0x6;
+            var CountOfCardsInArchetypes = new List<ushort>();
+            for (var Archetype = 0; Archetype < Archetypes; Archetype++)
+            {
+                var NumberOfCardsInArchetype = Reader.ReadUInt16();
+                var SumOfCards = Reader.ReadUInt16();
+
+                CountOfCardsInArchetypes.Add(NumberOfCardsInArchetype);
+
+                if (SumOfCards == NumberOfCards)
+                    break;
+            }
+
+            Reader.ReadUInt16();
+
+            for (ushort Archetype = 0; Archetype < CountOfCardsInArchetypes.Count; Archetype++)
+            {
+                var TempList = new List<int>();
+
+                for (var Card = 0; Card < CountOfCardsInArchetypes[Archetype]; Card++)
+                {
+                    TempList.Add(Reader.ReadUInt16());
+                }
+
+                CardsInArchetype.Add((Archetype)Archetype, TempList);
+            }
         }
 
         public static void Save()
