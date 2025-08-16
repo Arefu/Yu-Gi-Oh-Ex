b2375526bc1d98c2b645ff269da235e239d58f11
Johnathon <JohnathonKM@Gmail.com>
2025-08-16 12:51:22 +1200

    feat: Started  adding YDC saving, see issue #12

--- File Type Libraries/YDC/YDC.cs
+++ File Type Libraries/YDC/YDC.cs
@@ -5,15 +5,19 @@
         public static short MAX_NUMBER_OF_CARDS_IN_DECK = 60;
         private static short MAX_NUMBER_OF_CARDS_IN_SIDE_DECK = 15;
         private static short MAX_NUMBER_OF_CARDS_IN_EXTRA_DECK = 15;
+        private static string DECK_NAME { get; set; }
 
         public static short NumberOfCardsInDeck;
         public static short NumberOfCardsInSideDeck;
         public static short NumberOfCardsInExtraDeck;
 
         public static List<short> CardsInMainDeck = [];
+        public static List<short> CardsInSideDeck = [];
+        public static List<short> CardsInExtraDeck = [];
 
         public static List<short> Load(string path)
         {
+            DECK_NAME = new FileInfo(path).Name;
             short CurrentlyRead = 0;
 
             using (var Reader = new BinaryReader(File.Open(path, FileMode.Open)))
@@ -27,6 +31,19 @@
                 }
             }
             return CardsInMainDeck;
+        }
+
+        public static void Save()
+        {
+            if (string.IsNullOrEmpty(DECK_NAME)) return;
+
+            using var writer = new BinaryWriter(File.Open(DECK_NAME, FileMode.Create));
+            writer.Write(new byte[8]);
+
+            writer.Write((short)CardsInMainDeck.Count);
+
+            foreach (var card in CardsInMainDeck)
+                writer.Write(card);
         }
     }
 }
