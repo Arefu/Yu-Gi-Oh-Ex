7ef4ead59e50f72fb8826a5637cc7fdb8f59fcbd
Johnathon <JohnathonKM@Gmail.com>
2025-08-25 21:06:51 +1200

    feat: W.I.P tool for effect editing

---
+++ Tools/Millennium-UI/MillenniumUI.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Millennium_UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            button4 = new Button();
+            CTMYD_Open = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            dataGridView1 = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            tabPage2 = new TabPage();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(1264, 681);
+            tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(button4);
+            tabPage1.Controls.Add(CTMYD_Open);
+            tabPage1.Controls.Add(button2);
+            tabPage1.Controls.Add(button1);
+            tabPage1.Controls.Add(dataGridView1);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(1256, 653);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Cards That Make You Draw";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            button4.Location = new Point(451, 615);
+            button4.Name = "button4";
+            button4.Size = new Size(120, 30);
+            button4.TabIndex = 6;
+            button4.Text = "Save Schema";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // CTMYD_Open
+            // 
+            CTMYD_Open.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            CTMYD_Open.Location = new Point(325, 615);
+            CTMYD_Open.Name = "CTMYD_Open";
+            CTMYD_Open.Size = new Size(120, 30);
+            CTMYD_Open.TabIndex = 5;
+            CTMYD_Open.Text = "Open Schema";
+            CTMYD_Open.UseVisualStyleBackColor = true;
+            CTMYD_Open.Click += CTMYD_Open_Click;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            button2.Location = new Point(134, 615);
+            button2.Name = "button2";
+            button2.Size = new Size(120, 30);
+            button2.TabIndex = 4;
+            button2.Text = "Remove Card";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            button1.Location = new Point(8, 615);
+            button1.Name = "button1";
+            button1.Size = new Size(120, 30);
+            button1.TabIndex = 3;
+            button1.Text = "Add Card";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2 });
+            dataGridView1.Location = new Point(8, 6);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(563, 603);
+            dataGridView1.TabIndex = 1;
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "Card ID";
+            Column1.Name = "Column1";
+            Column1.ToolTipText = "The Konami ID of the Card you wish to set";
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "Draw Count";
+            Column2.Name = "Column2";
+            Column2.ToolTipText = "The number of Cards that the ID should Draw.";
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(1256, 653);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1264, 681);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "Millennium UI";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private Button button1;
+        private DataGridView dataGridView1;
+        private TabPage tabPage2;
+        private Button button2;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private Button button4;
+        private Button CTMYD_Open;
+    }
+}
