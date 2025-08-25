de27ce7e20d0dcb57701d1ac5035607cb1c2a92b
Johnathon <JohnathonKM@Gmail.com>
2025-08-25 21:07:01 +1200

    fix: add vector handler as well

--- Yu-Gi-Oh_Loader Plugins/Yu-Gi-Oh-PatchMeOut/dllmain.cpp
+++ Yu-Gi-Oh_Loader Plugins/Yu-Gi-Oh-PatchMeOut/dllmain.cpp
@@ -81,6 +81,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
 	Logger::SetupLogger();
 	ProcessConfig();
 	SetUnhandledExceptionFilter(CrashHandler);
+	AddVectoredExceptionHandler(1, CrashHandler);
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
