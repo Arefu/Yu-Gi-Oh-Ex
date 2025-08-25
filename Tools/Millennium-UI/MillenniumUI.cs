3c0cc788a85fa348768481b505096cb00f1ba552
Johnathon <JohnathonKM@Gmail.com>
2025-08-25 21:06:51 +1200

    feat: W.I.P tool for effect editing

---
+++ Tools/Millennium-UI/MillenniumUI.cs
@@ -0,0 +1,14 @@
+namespace Millennium_UI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CTMYD_Open_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
