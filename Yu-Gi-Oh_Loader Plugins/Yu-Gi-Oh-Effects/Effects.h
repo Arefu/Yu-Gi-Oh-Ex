3085223e9dce1bb0773a987f98868c59ac66cf7d
Johnathon <JohnathonKM@Gmail.com>
2026-01-16 17:40:34 +1300

    feat: Continued work on New cards

--- Yu-Gi-Oh_Loader Plugins/Yu-Gi-Oh-Effects/Effects.h
+++ Yu-Gi-Oh_Loader Plugins/Yu-Gi-Oh-Effects/Effects.h
@@ -10,31 +10,63 @@
 #include "Config.h"
 #include "Effects.h"
 
-class EffectTable_SpellAndTrap
+class Effects_FunctionTable
 {
 public:
-	struct Table {
-		uintptr_t KonamiID;
-		void* FunctionOne;
-		void* FunctionTwo;
-		void* FunctionThree;
-		void* FunctionFour;
-		void* FunctionFive;
+    struct Table_01 {
+        short KonamiID;
+        short UNK01;
+        short UNK02;
+        short UNK03;
 
-		Table(uintptr_t id = 0, void* f1 = nullptr, void* f2 = nullptr, void* f3 = nullptr, void* f4 = nullptr, void* f5 = nullptr)
-			: KonamiID(id),
-			FunctionOne(f1),
-			FunctionTwo(f2),
-			FunctionThree(f3),
-			FunctionFour(f4),
-			FunctionFive(f5)
-		{
-		}
-	};
-	static std::vector<Table> EffectTable;
+        void* FunctionOne;
+        void* FunctionTwo;
+        void* FunctionThree;
+        void* FunctionFour;
+        void* FunctionFive;
 
-	static void SetupInternalEffectTable(uintptr_t Address);
-	static void InsertRedirects(uintptr_t Address);
-	static void AdjustTable();
-	static void CopyTable(uintptr_t Address);
+        Table_01(uintptr_t id = 0, void* f1 = nullptr, void* f2 = nullptr, void* f3 = nullptr, void* f4 = nullptr, void* f5 = nullptr)
+            : KonamiID(id), FunctionOne(f1), FunctionTwo(f2), FunctionThree(f3), FunctionFour(f4), FunctionFive(f5) {
+        }
+    };
+    struct Table_02 {
+        uintptr_t KonamiID;
+
+        void* FunctionOne;
+        void* FunctionTwo;
+        void* FunctionThree;
+        void* FunctionFour;
+        void* FunctionFive;
+    };
+
+    struct Table_05 {
+        uintptr_t KonamiID;
+        void* FunctionOne;
+        void* FunctionTwo;
+        void* FunctionThree;
+        void* FunctionFour;
+        void* FunctionFive;
+
+        Table_05(uintptr_t id = 0, void* f1 = nullptr, void* f2 = nullptr, void* f3 = nullptr, void* f4 = nullptr, void* f5 = nullptr)
+            : KonamiID(id), FunctionOne(f1), FunctionTwo(f2), FunctionThree(f3), FunctionFour(f4), FunctionFive(f5) {
+        }
+    };
+
+    //0x140B38290
+    static std::vector<Table_01> EffectTable_1;
+
+    //0x0140B5CC20
+    static std::vector<Table_02> EffectCards_FunctionTable;
+
+    static std::vector<Table_05> EffectTable_5;
+    /// <summary>
+    /// Entry Point - Redirects Vectors
+    ///
+    static void Setup();
+
+    static void DeleteItemFromList(short KonamiID);
+
+    static void InsertRedirects();
+    static void AdjustTable();
+    static void CopyTable(uintptr_t Address);
 };
--- Yu-Gi-Oh_Loader Plugins/Yu-Gi-Oh-Effects/dllmain.cpp
+++ Yu-Gi-Oh_Loader Plugins/Yu-Gi-Oh-Effects/dllmain.cpp
@@ -11,35 +11,40 @@
 #include "Effects.h"
 #include "Logger.h"
 #include "Config.h"
+#include "File.h"
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
-	Logger::SetupLogger();
+    Logger::SetupLogger();
 
-	std::string Path = "C:\\";
+    std::string Path = "C:\\";
 
-	switch (ul_reason_for_call)
-	{
-	case DLL_PROCESS_ATTACH:
-		Path = Config::Get_WorkingDirectory();
-		if (Path == "")
-		{
-			char Buffer[256];
-			MessageBoxA(NULL, "Failed To Load/Parse Config.ini, Please Make Sure It Exists/ Is Setup And In The Same Directory As The Game!", "Yu-Gi-Oh-Effects", MB_ICONERROR);
-		}
-		Logger::WriteLog(std::format("Running from: {}", Path), MODULE_NAME, 0);
-		DetourTransactionBegin();
-		DetourUpdateThread(GetCurrentThread());
-		Table_140B15B30::Setup();
-		Table_140B15B30::Patch();
-		EffectTable_SpellAndTrap::SetupInternalEffectTable(0x140BA8730);
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        Path = Config::Get_WorkingDirectory();
+        if (Path == "")
+        {
+            char Buffer[256];
+            MessageBoxA(NULL, "Failed To Load/Parse Config.ini, Please Make Sure It Exists/ Is Setup And In The Same Directory As The Game!", "Yu-Gi-Oh-Effects", MB_ICONERROR);
+        }
+        Logger::WriteLog(std::format("Running from: {}", Path), MODULE_NAME, 0);
 
-		CardsThatMakeYouDraw::Setup(std::format("{}{}", Path, "\\CardsThatMakeYouDraw\\CardsThatMakeYouDraw.json"));
+        DetourTransactionBegin();
+        DetourUpdateThread(GetCurrentThread());
 
-		EffectTable_SpellAndTrap::InsertRedirects(0x140BA8730);
-		DetourTransactionCommit();
+        Effects_FunctionTable::Setup();
 
-		break;;
-	}
-	return TRUE;
+        Table_140B15B30::Setup();
+        Table_140B15B30::AlterTable(FileIO::Read_FromEffectFile(std::format("{}{}", Path, "\\140B15B30\\140B15B30.json")));
+        Table_140B15B30::Patch();
+        Effects_FunctionTable::AdjustTable();
+        //CardsThatMakeYouDraw::Setup(std::format("{}{}", Path, "\\CardsThatMakeYouDraw\\CardsThatMakeYouDraw.json"));
+
+        Effects_FunctionTable::InsertRedirects();
+        DetourTransactionCommit();
+
+        break;;
+    }
+    return TRUE;
 }
