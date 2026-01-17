b42d3e3cce8b4568f8a2555f4e0bca1750fb15ff
Johnathon <JohnathonKM@Gmail.com>
2026-01-17 21:39:00 +1300

    feat: Added some more functions from areas of the game

--- Dependencies/Yu-Gi-Oh-Ex/YuGiOh/YuGiOh-CARDS.h
+++ Dependencies/Yu-Gi-Oh-Ex/YuGiOh/YuGiOh-CARDS.h
@@ -1,6 +1,24 @@
+#include <Windows.h>
+
 namespace YGO
 {
     namespace CARDS
     {
+        inline auto Get_CardNameFromKonamiId = reinterpret_cast<LPCTSTR(__fastcall*)(short KonamiID)>(0x14076D0F0);
+        inline auto Get_CardDescriptionFromKonamiId = reinterpret_cast<LPCTSTR(__fastcall*)(short KonamiID)>(0x14076D070);
+        inline auto Get_EffectiveAttackFromKonamiId = reinterpret_cast<int(__fastcall*)(short KonamiID)>(0x1407CA6F0);
+        inline auto Get_RawAttackFromKonamiId = reinterpret_cast<int(__fastcall*)(short KonamiID)>(0x1407CA6B0);
+        inline auto Get_EffectiveDefenseFromKonamiId = reinterpret_cast<int(__fastcall*)(short KonamiID)>(0x1407CA7C0);
+        inline auto Get_CardLimitedStatusFromKonamiId = reinterpret_cast<int(__fastcall*)(short KonamiID)>(0x1407CA910);
+        inline auto Get_CardLevelFromKonamiId = reinterpret_cast<int(__fastcall*)(short KonamiID)>(0x1407CAA60);
+        inline auto Get_CardTypeFromKonamiId = reinterpret_cast<int(__fastcall*)(short KonamiID)>(0x1407CAA90);
+        inline auto Get_CardSameFromKonamiId = reinterpret_cast<int(__fastcall*)(short KonamiID)>(0x14076D610);
+        inline auto Get_CurrentCardId = reinterpret_cast<short(__fastcall*)()>(0x14076D630);
+
+        inline auto Get_InternalIdFromKonamiId = reinterpret_cast<short(__fastcall*)(short KonamiID)>(0x14076E000);
+
+        inline auto Is_ValidCardId = reinterpret_cast<bool(__fastcall*)(short KonamiID)>(0x1407CA820);
+
+        inline auto Get_XX = reinterpret_cast<int(__fastcall*)(short KonamiID)>(0x140753BA0);
     }
 }
--- Dependencies/Yu-Gi-Oh-Ex/YuGiOh/YuGiOh-DUEL.h
+++ Dependencies/Yu-Gi-Oh-Ex/YuGiOh/YuGiOh-DUEL.h
@@ -2,43 +2,48 @@ namespace YGO
 {
     namespace DUEL
     {
-        /// <summary>
-        /// Returns the number of Players for the current duel type.
-        /// </summary>
-        /// <returns>2 or 4 Players</returns>
+        inline auto Get_DuelStuck = reinterpret_cast<bool(*)()>(0x1407AB080);
+        inline auto Set_DuelStuck = reinterpret_cast<void(*)()>(0x1407AB170);
+
+        inline auto Get_IsDuelMultiplayer = reinterpret_cast<short(*)()>(0x1407691D0);
+        inline auto Set_IsDuelMultiplayer = reinterpret_cast<void(*)(bool IsMultiplayer)>(0x140769720);
+
+        inline auto Get_IsRoundBasedDuel = reinterpret_cast<short(*)()>(0x1407691F0);
+        inline auto Set_IsRoundBasedDuel = reinterpret_cast<void(*)(bool IsRoundBasedDuel)>(0x140769830);
+
+        inline auto Get_IsTutorialDuel = reinterpret_cast<short(*)()>(0x1407694F0);
+        inline auto Set_IsTutorialDuel = reinterpret_cast<bool(*)(bool IsTutorialDuel)>(0x140769B00);
+
+        inline auto Get_StartingLifePoints = reinterpret_cast<int(*)()>(0x140768E30);
+        inline auto Set_StartingLifePoints = reinterpret_cast<void(*)(int LifePoints)>(0x140769710);
+
+        inline auto Get_StartingPlayer = reinterpret_cast<int(*)()>(0x140768DE0);
+        inline auto Set_StartingPlayer = reinterpret_cast<void(*)(int StartingPlayer)>(0x140769740);
+
+        inline auto Get_TutorialDuelIndex = reinterpret_cast<int(*)()>(0x140769180);
+        inline auto Set_TutorialDuelIndex = reinterpret_cast<int(*)(int TutorialIndex)>(0x140769B20);
+
+        inline auto Get_DuelTimeLimit = reinterpret_cast<int(*)()>(0x140768E10);
+        inline auto Set_DuelTimeLimit = reinterpret_cast<void(*)(int DuelTimeLimitInMilliseconds)>(0x140769750);
+
+        inline auto Get_IsTagDuel = reinterpret_cast<bool(*)()>(0x1407694D0);
+        inline auto Set_IsTagDuel = reinterpret_cast<void(*)(bool IsTagDuel)>(0x140769AC0);
+
+        inline auto Get_DefaultStartingLifePoints = reinterpret_cast<int(*)(int Index)>(0x1407F1E40);
+
         inline auto Get_NumberOfPlayers = reinterpret_cast<int(*)()>(0x140768DF0);
 
-        /// <summary>
-        /// Returns if the current duel is a Tutorial Duel.
-        /// </summary>
-        /// <returns></returns>
-        inline auto Get_IsTutorialDuel = reinterpret_cast<short(*)()>(0x1407694F0);
+        inline auto Is_DuelTimerIncriment = reinterpret_cast<bool(*)(int Increment)>(0x1407694C0);
 
-        /// <summary>
-        /// Sets if the current duel is a Tutorial Duel.
-        /// </summary>
-        /// <returns></returns>
-        inline auto Set_IsTutorialDuel = reinterpret_cast<bool(*)(bool)>(0x140769B00);
-
-        /// <summary>
-        /// Sets the Tutorial Duel Index.
-        /// </summary>
-        /// <returns></returns>
-        inline auto Set_TutorialDuelIndex = reinterpret_cast<int(*)(int)>(0x140769B20);
-
-        /// <summary>
-        /// Address for whether the current duel is a Tag Duel.
-        /// </summary>
         inline bool& bIsTagDuel = *reinterpret_cast<bool*>(0x140C8D35D);
-
-        /// <summary>
-        /// Addres for whether the current duel is a Tutorial Duel.
-        /// </summary>
         inline bool& bIsTutorialDuel = *reinterpret_cast<bool*>(0x140C8D1EA);
+        inline bool& bIsDuelMultiplayer = *reinterpret_cast<bool*>(0x140C8D1E6);
+        inline bool& bIsRoundBasedDuel = *reinterpret_cast<bool*>(0x140C8D35C);
+        inline bool& bSetDuelStuck = *reinterpret_cast<bool*>(0x140D4FCFB);
 
-        /// <summary>
-        /// Address for the Tutorial Duel Index.
-        /// </summary>
         inline int& iTutorialDuelIndex = *reinterpret_cast<int*>(0x140C8D1EC);
+        inline int& iDuelTimeLimit = *reinterpret_cast<int*>(0x140C8D368);
+        inline int& iDuelTimerIncrement = *reinterpret_cast<int*>(0x140C8D364);
+        inline int& iSTartingLifePoints = *reinterpret_cast<int*>(0x140C8D370);
     }
 }
